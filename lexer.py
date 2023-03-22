c6065ce5fb5a3844b4f133654efbe5bb83e4a8b1
Katharsis0 <hvsebas@estudiantec.cr>
2023-03-21 23:09:53 -0600

    Added Validation for ID Prod Rule

--- lexer.py
+++ lexer.py
@@ -54,7 +54,7 @@ t_GT=r'>'
 t_LT=r'<'
 t_COMMA=r','
 t_SEMICOLON=r';'
-#t_PRINT=r'=>\s*(.*)'
+t_PRINT=r'=>'
 
 #regular expression rules for matching tokens with actions
 
@@ -123,7 +123,7 @@ lexer=lex.lex()
 
 
 #Input extraido del IDE
-lexer.input("""@Principal""")
+lexer.input("""@iden2#_""")
 
 #Testing, this should be sent to the IDEs terminal
 while True:
--- parse.py
+++ parse.py
@@ -35,7 +35,10 @@ def p_def_rule(p):
                  | DEF LP ID COMMA TYPE COMMA BOOl RP SEMICOLON
                  | DEF LP ID COMMA TYPE COMMA INT RP SEMICOLON
     '''
-    p[0]=('assign',p[1],p[3])
+    if len(p)==7:
+        p[0]=('assign',p[3],None)
+    elif len(p)==9:
+        p[0]=('assign',p[3],p[7])
 
 #Definition of ID(val)
 def p_id_rule(p):
@@ -43,10 +46,41 @@ def p_id_rule(p):
                  | ID LP ID RP SEMICOLON
                  | ID LP INT PLUS ID RP SEMICOLON
     '''
-    p[0]=('assign',p[1],p[3])
+    if isinstance(p[3],int) and isinstance(p[5],int):
+        res =p[3]+p[5]
+        p[0]=('assign',p[1],res)
+    else:
+        p[0]=('assign',p[1],p[3])
 
-#definition of Alter(id,val)
+#definition of Alter(id,val) m
 def p_alter_rule(p):
     ''' sentence : ALTER LP ID COMMA INT RP SEMICOLON
     '''
     p[0]=('assign',p[1],p[3])
+
+#definition of expression
+def p_expression_rule(p):
+    '''expression: term
+                  | expression PLUS term
+                  | expression MINUS term
+                  | expression STAR term
+                  | expression SLASH term
+                
+    '''
+    if len(p) == 2:
+        p[0] = p[1]
+    elif p[2] == '+':
+        p[0] = ('add', p[1], p[3])
+    elif p[2] == '-':
+        p[0] = ('sub', p[1], p[3])
+    elif p[2] == '*':
+        p[0] = ('mul', p[1], p[3])
+    elif p[2] == '/':
+        p[0] = ('div', p[1], p[3])
+
+def p_print_rule(p):
+    ''' sentence : PRINT LP ID RP SEMICOLON
+                 | PRINT LP INT RP SEMICOLON
+                 | PRINT LP BOOL RP SEMICOLON
+    '''
+    p[0]=('print',p[3])
